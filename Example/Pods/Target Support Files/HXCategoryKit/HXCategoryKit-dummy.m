baa8cb1208499bfd1d3c4dd3b68165cc5421bf27
BBJMikey <605055291@qq.com>
2018-01-05 14:47:18 +0800

    init

---
+++ Example/Pods/Target Support Files/HXCategoryKit/HXCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HXCategoryKit : NSObject
+@end
+@implementation PodsDummy_HXCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/HXCategoryKit/HXCategoryKit-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "JXLayoutButton.h"
+#import "TFHpple.h"
+#import "TFHppleElement.h"
+#import "XPathQuery.h"
+#import "UINavigationController+FDFullscreenPopGesture.h"
+
+FOUNDATION_EXPORT double HXCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char HXCategoryKitVersionString[];
+
